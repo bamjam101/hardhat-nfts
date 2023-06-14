f7aaf92d5994dd2ed3e6b01d4005bc4a019d731b
bamjam101 <herejamil123@gmail.com>
2023-06-14 16:13:18 +0530

    Initial commit, hardhat project setup for developing and deploying NFTs.

---
+++ hardhat.config.js
@@ -0,0 +1,76 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("dotenv").config()
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const MAINNEET_RPC_URL = process.env.MAINNEET_RPC_URL
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+      forking: {
+        url: MAINNEET_RPC_URL,
+      },
+    },
+    sepolia: {
+      chainId: 11155111,
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      blockConfirmations: 6,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+    customChains: [
+      {
+        network: "goerli",
+        chainId: 5,
+        urls: {
+          apiURL: "https://api-goerli.etherscan.io/api",
+          browseURL: "https://goerli.etherscan.io",
+        },
+      },
+    ],
+  },
+  gasReporter: {
+    enabled: false,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+      },
+      { version: "0.6.12" },
+      {
+        version: "0.4.19",
+      },
+      { version: "0.6.0" },
+    ],
+  },
+  mocha: {
+    timeout: 200000, // 200 seconds max
+  },
+}
